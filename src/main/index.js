25398427ea7a4862526937ce39f90c1fd1f054e9
SqGoliaf <ivanov@v-1336.ru>
2023-04-20 15:15:01 +0300

    Little finish

--- src/main/index.js
+++ src/main/index.js
@@ -1,3 +1,40 @@
-import { startServer } from 'server/server.js';
+const express = require('express');
+const path = require('path');
+const port = parseArg();
+const app = express();
 
-startServer()
+// Включение сервера
+app.use(express.static(path.join(__dirname, 'public')));
+app.get("/", (req, res) => {
+    res.sendFile(`${__dirname}/ui/index.html`);
+});
+app.listen(port, () => {
+    console.log(`Веб приложение запущено на http://localhost:${port}`);
+});
+
+
+// Парсинг аргументов
+function parseArg() {
+    const yargs = require('yargs');
+
+    yargs.usage('Usage: $0 --port [num]')
+        .version('1.0.0')
+        .option('p', {
+            alias: 'port',
+            descride: "This is port connect",
+            type: 'number',
+            default: 337,
+            nargs: 1,
+        })
+        .help('h')
+        .alias('h', 'help')
+        .alias('v', 'version')
+        .epilog('Copyright 2023')
+        .example('$0 --port 442',
+            'Server connect on the port')
+        .argv;
+
+    const port = yargs.argv['port'];
+
+    return typeof port === 'number' ? port : 337;
+}
---
+++ src/main/logic/windows/users.js
@@ -0,0 +1,28 @@
+const
+    iconv = require('iconv-lite'),
+    { exec } = require('child_process')
+
+const exec_net_user = (callback) => {
+    exec('net user', {encoding: 'buffer'},
+        (error, stdout, stderr) => {
+            if (error) {
+                callback(stderr, null);
+            } else {
+                callback(null, iconv.decode(stdout, 'CP866'));
+            }
+        }
+    );
+}
+
+const users = (callback) => (exec_net_user((err, res) => {
+    if (err) {
+        console.log(`Error: ${err}`);
+    } else {
+        callback(res
+            .slice(res.lastIndexOf('-') + 1, res.indexOf('Команда выполнена успешно'))
+            .match(/([A-Za-zА-Яа-я0-9_]+)/g)
+        );
+    }
+}));
+
+module.exports = users;
